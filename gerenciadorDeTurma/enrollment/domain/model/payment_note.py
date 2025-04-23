f0333ca53d6939485722ae720e84030325793837
Osvaldo de Sousa <74511769+Osvaldo943@users.noreply.github.com>
2025-04-23 17:46:25 +0100

    Delete gerenciadorDeTurma directory

--- gerenciadorDeTurma/enrollment/domain/model/payment_note.py
+++
@@ -1,5 +0,0 @@
-class PaymentNote():
-    def __init__(self, idStudent, price, dueDate):
-        self.idStudent = idStudent
-        self.price = price
-        self.dueDate = dueDate
--- gerenciadorDeTurma/enrollment/domain/ports/enrollment.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from enrollment.domain.model.student import Student
-
-class IEnrollment(ABC):
-    
-    @abstractmethod
-    def enroll(self, student: Student):
-        pass
